a00099b5b15afa2e922225d0298c86e47e63906f
nate <niibobby4@gmail.com>
2022-02-05 23:25:27 +0000

    added the link to the js file

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ const path = require("path")
 const app=express()
 const PORT = 5000
 
-app.set("view engine", "ejs")
-app.use("/css", express.static(path.resolve(__dirname,"assets/css")))
+app.use (bodyparser.json())
 
+//set view engine
+app.set("view engine", "ejs")
+
+//setup paths for static assests and files
+app.use("/css", express.static(path.resolve(__dirname,"assets/css")))
+app.use("/js", express.static(path.resolve(__dirname,"assets/js/")) )
+
+
+//render the registration page on page load
 app.get("/", (req, res)=>{
-    res.render("index")
+    res.render("index") 
 })
 app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`))
