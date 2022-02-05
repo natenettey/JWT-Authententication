8aeaf88c70eff5a6321c10e40df75d17ffbbd13e
nate <niibobby4@gmail.com>
2022-02-05 19:52:59 +0000

    setup nodemon and linked css file to ejs header file

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const path = require("path")
 const app=express()
 const PORT = 5000
 
+app.set("view engine", "ejs")
+app.use("/css", express.static(path.resolve(__dirname,"assets/css")))
+
 app.get("/", (req, res)=>{
-    res.render()
+    res.render("index")
 })
 app.listen(PORT, console.log(`Server running on http://localhost:${PORT}`))
