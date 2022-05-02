31cc532ed2a03b952e72e7949938e39a792a4ad9
nate <niibobby4@gmail.com>
2022-05-02 16:24:50 +0000

    added the login and index page routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const render = require('../render')
+const controller = require('./../../controller/controller')
 
 
 router.get('/', render.home_page)
+router.post('/api/register', controller.register)
 
 module.exports = router
