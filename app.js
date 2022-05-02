80d17c1c60b5e7d935f5697087d4782ca0a26da0
nate <niibobby4@gmail.com>
2022-05-02 17:39:30 +0000

    stripped down file

--- app.js
+++ app.js
@@ -18,93 +18,18 @@ const JWT_SECRET ="sdgsdfdvvsv%^$^%@#%^$&*&^%%^utfugvfujuihJ:>?>?>?<>}{}{p)_)_*(
 const app=express()
 dotenv.config({path:"config.env"})
 const PORT = 5000
-
 //db connection
 connect_db()
-
 //body parser
 app.use (bodyparser.json())
-
 //set view engine
 app.set("view engine", "ejs")
-
 //setup paths for static assests and files
 app.use("/css", express.static(path.resolve(__dirname,"assets/css/")))
 app.use("/js", express.static(path.resolve(__dirname,"assets/js/")) )
-
-
 //load routes
 app.use('/', require('./server/routes/routes'))
 
-//logic for registration page
-app.post("/api/register", async(req,res)=>{
-    console.log(req.body)
-   const {username, email, residence, password:plainTextPassword} = req.body
-
-   //check if all inputs have been received
-   if (!username || typeof username !== 'string'){
-       res.json({error:"Invalid Username"})
-   }else if(!email){
-      res.json({error:"Please add an email address"})
-   }else if(!residence){
-    res.json({error:"Please add a residence"})
- }else if(!plainTextPassword){
-    res.json({error:"Please add a password"})
- }
-
-   //hash the password
-   const password = await bcrypt.hash(plainTextPassword, 10)
-   console.log(password, email) 
-
-   //create new user
-   try {
-      const response =  await userModel.create({
-           username,
-           email,
-           residence,
-           password
-       })
-       console.log("User created", response)
-   } catch (error) {
-       console.log(error)// can use""error.message"
-       if (error.code === 11000){
-           return res.json({error:"Email already in use ! "})
-       }
-       throw error
-   }
-   res.json({status:'ok'})
-})
-
-
-
-//render login page
-app.get("/login", (req, res)=>{
-    res.render("login")
-})
-
-//logic for login page
-app.post('/api/login', async(req, res)=>{
-    const {email, password}  = req.body
-
-    //get user with the passed email and password
-    const user = await userModel.findOne({email}).lean()
-    if(!user){
-        res.json({status:"error", error:"Invalid username / password"})
-    }
-
-    //check if the passwords match
-    if(await bcrypt.compare(password, user.password)){
-
-        const token = jwt.sign(
-            {
-                id:user._id,
-                username:user.username
-            },JWT_SECRET
-        )
-        return res.json({status: "ok", data:token})
-    }
-    return res.json({status:"error", error:"Invalid username/password"})
-})
 
 
 //CONFIGURE MULTER FOR STORING FILE UPLOADS
@@ -117,11 +42,10 @@ const storage  = multer.diskStorage({
         cb(null, file.fieldname + '-' + Date.now())
     }
 })
-
 const upload = multer({storage:storage})
 
 
-// Step 8 - the POST handler for processing the uploaded file
+// the POST handler for processing the uploaded file
   
 app.post('/api/upload', upload.single('image'), (req, res, next) => {
   
