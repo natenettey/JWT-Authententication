82f0d479216197ed0b179a28d462956e5b638fbe
nate <niibobby4@gmail.com>
2022-05-02 18:03:34 +0000

    added the login route

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const render = require('../render')
+const multer  = require ('multer')
 const controller = require('./../../controller/controller')
 
-
+//set routes
 router.get('/', render.home_page)
 router.post('/api/register', controller.register)
+router.get('/login', render.login_page)
+router.post('/api/login', controller.login)
+// router.post('/api/login', controller.login)
+
 
 module.exports = router
+
