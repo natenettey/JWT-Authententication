5e9217a39c274ad10de9fa66c7f725949be4a940
nate <niibobby4@gmail.com>
2022-05-02 17:41:21 +0000

    added the login code

--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,15 @@
 const userModel = require('../model/user_model')
 const bcrypt = require('bcryptjs')
+const JWT_SECRET ="sdgsdfdvvsv%^$^%@#%^$&*&^%%^utfugvfujuihJ:>?>?>?<>}{}{p)_)_*(&^#$$%cgfvghv"
+const jwt  = require ("jsonwebtoken")
+const fs = require('fs')
+const path = require("path")
 
 
-//LOGIC FOR REGISTRATION PAGE
+
+
+
+//-------LOGIC FOR REGISTRATION PAGE---------
 exports.register = async(req,res)=>{
     console.log(req.body)
    const {username, email, residence, password:plainTextPassword} = req.body
@@ -40,4 +47,33 @@ exports.register = async(req,res)=>{
    }
    res.json({status:'ok'})
 }
-// app.post("/api/register", )
+
+
+
+//------LOGIC FOR LOGIN PAGE-------
+exports.login = async(req, res)=>{
+    const {email, password}  = req.body
+
+    //get user with the passed email and password
+    const user = await userModel.findOne({email}).lean()
+    if(!user){
+        res.json({status:"error", error:"Invalid username / password"})
+    }
+
+    //check if the passwords match
+    if(await bcrypt.compare(password, user.password)){
+
+        const token = jwt.sign(
+            {
+                id:user._id,
+                username:user.username
+            },JWT_SECRET
+        )
+        return res.json({status: "ok", data:token})
+    }
+    return res.json({status:"error", error:"Invalid username/password"})
+}
+
+
+
+
